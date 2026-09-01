1a4bd654e621fd7c3c004abb8b4ebd8202395a7f
Tosin <56510627+TosinA91@users.noreply.github.com>
2026-09-01 19:32:56 -0400

    Updated lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps('Hello from out CICD Github actions workflow vscode')
+        "body": json.dumps('Hello from my CICD Github actions workflow vscode')
     }
